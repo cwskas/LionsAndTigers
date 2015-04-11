3facc3e562f6aed754be645cc5ec01b2321e1133
cwskas <cwskas@gmail.com>
2015-04-11 06:04:52 -0500

    1st commit, works

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,19 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Willie Strickland on 4/11/15.
+//  Copyright (c) 2015 Willie Strickland. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// the 'blueprint for our tiger
+struct Tiger {
+//    <#properties and methods#>
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        println("My Tiger's name is \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed), and its image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "Age \(myTiger.age)"
+        breedLabel.text = myTiger.breed
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonPressed(sender: UIBarButtonItem) {
+    }
 
 }
 
